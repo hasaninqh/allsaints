34170a605fefe5ee7d33ccd9620a7dd9c4f51ae8
hasanein <hasaninqh@yahoo.com>
2019-01-27 12:21:21 +0000

    create new modal not functioninig properly

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,11 +1,301 @@
-import React from 'react'
+import React, { Component } from 'react'
+import "../App.css"
+import { withStyles } from '@material-ui/core/styles';
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogContent from '@material-ui/core/DialogContent';
+import image1 from "../images/all_saints_pre-school_hw_2016_17007001.jpg";
+import image2 from "../images/all_saints_pre-school_hw_2016_17007002.jpg";
+import image3 from "../images/all_saints_pre-school_hw_2016_17007003.jpg";
+import image4 from "../images/all_saints_pre-school_hw_2016_17007004.jpg";
+import image5 from "../images/all_saints_pre-school_hw_2016_17007005.jpg";
+import image6 from "../images/all_saints_pre-school_hw_2016_17007006.jpg";
+import image7 from "../images/all_saints_pre-school_hw_2016_17007007.jpg";
+import image8 from "../images/all_saints_pre-school_hw_2016_17007008.jpg";
+import image9 from "../images/all_saints_pre-school_hw_2016_17007009.jpg";
+import image10 from "../images/all_saints_pre-school_hw_2016_17007010.jpg";
+import image11 from "../images/all_saints_pre-school_hw_2016_17007011.jpg";
+import image12 from "../images/all_saints_pre-school_hw_2016_17007012.jpg";
+import image13 from "../images/all_saints_pre-school_hw_2016_17007013.jpg";
+import image14 from "../images/all_saints_pre-school_hw_2016_17007014.jpg";
+import image15 from "../images/all_saints_pre-school_hw_2016_17007015.jpg";
+import image16 from "../images/all_saints_pre-school_hw_2016_17007028.jpg";
+const DialogContent = withStyles(theme => ({
+  root: {
+    margin: 0,
+    padding: theme.spacing.unit * 2,
+  },
+}))(MuiDialogContent);
 
-const Gallery = () => {
-  return (
-    <div>
+
+
+export default class Gallery extends Component {
+  state={
+   open : false,
+  }
+
+   handleClick = ()=>{
+     // var modal = document.getElementById('myModal');
+      this.myModal.style.display = "block";
+      //modalImg.src = this.props.src;
+   
+   }
+  
+   handleClose =()=>{
+       //var modal = document.getElementById('myModal');
+       this.myModal.style.display = "none";
+      }
+  
+  
+   
+
+  
+ 
+  render() {
+    return (
+       <div className="row">
+         
+          <div className="responsive">
+             <div className="gallery">
+        
+                 <img id="myImg" src={image1} alt="Cinque Terre"  onClick={this.handleClick.bind(this)} />
+                 {/* The Modal */}
+                  <div id="myModal" class="modal" ref={modal=>{this.myModal=modal}}>
+                       {/* The Close Button */}
+                       <span class="close" onClick={this.handleClose}>&times;</span>
+                       {/* Modal Content (The Image) */}
+                       <img src={image1} class="modal-content" id="img01" alt="children" />
+                       {/* Modal Caption (Image Text) */}
+                     
+                  </div>
+                
+             </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image2} alt="Cinque Terre" width="600" height="400" onClick={this.handleClick.bind(this)} />
+                 <div id="myModal" class="modal" ref={modal=>{this.myModal=modal}}>
+                       {/* The Close Button */}
+                       <span class="close" onClick={this.handleClose}>&times;</span>
+                       {/* Modal Content (The Image) */}
+                       <img src={image2} class="modal-content" id="img01" alt="children" />
+                       {/* Modal Caption (Image Text) */}
+                     
+                  </div>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                <img src={image3} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                     <DialogContent>
+                        <img src={image3} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image4} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                  <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                     <DialogContent>
+                        <img src={image5} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image6} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                     <DialogContent>
+                        <img src={image6} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image7} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                     <DialogContent>
+                        <img src={image7}alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image8} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                     <DialogContent>
+                        <img src={image8} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image9} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                      <DialogContent>
+                        <img src={image9} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image10} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                      <DialogContent>
+                        <img src={image10} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image11} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                      <DialogContent>
+                        <img src={image11} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image12} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                      <DialogContent>
+                        <img src={image12} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image13} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                      <DialogContent>
+                        <img src={image13} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image14} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                      <DialogContent>
+                        <img src={image14} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image15} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                      <DialogContent>
+                        <img src={image15} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+
+          <div class="responsive">
+           <div class="gallery">
+                 <img src={image16} alt="Cinque Terre" width="600" height="400" onClick={this.handleClickOpen} />
+                 <Dialog
+                  onClose={this.handleClose}
+                  aria-labelledby="customized-dialog-title"
+                  open={this.state.open}
+                  >
+                      <DialogContent>
+                        <img src={image16} alt="Cinque Terre" />
+                     </DialogContent>
+                  </Dialog>
+           </div>
+          </div> 
+       
+       <div class="clearfix"></div>
+
       
-    </div>
-  )
+        
+
+     </div>
+    )
+  }
 }
 
-export default Gallery
+
+
+
+
+
+
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,68 +1,72 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
 const Navigation = ({ fixedNav }) => {
   let classes =
-    "nav-wrapper center s12 m12 l12 pink lighten-5 blue-text text-darken-4";
+    "navWrapper lighten-5 blue-text text-darken-4";
   return (
-    <nav
-      className={fixedNav ? classes + " fixed-nav-top" : classes}
-      style={{ textAlign: "center" }}
-    >
-      <ul className="center blue-text text-lighten-4">
-        {fixedNav && (
-          <li>
-            <Link to="/" className="blue-text text-lighten-2">
-              IMAGE
-            </Link>
-          </li>
-        )}
-        <li>
-          <Link to="/" className="blue-text text-lighten-2">
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link to="/Earlyyears" className="blue-text text-lighten-2">
-            Early tyears foundation stage
-          </Link>
-        </li>
-        <li>
-          <Link to="/Ofested" className="blue-text text-lighten-2">
-            Ofsted
-          </Link>
-        </li>
-        <li>
-          <Link to="/Funding" className="blue-text text-lighten-2">
-            Funding
-          </Link>
-        </li>
-        <li>
-          <Link to="/Testimonials" className="blue-text text-lighten-2">
-            Testimonials
-          </Link>
-        </li>
-        <li>
-          <Link to="/Fundraising" className="blue-text text-lighten-2">
-            Fundraising
-          </Link>
-        </li>
-        <li>
-          <Link to="/" className="blue-text text-lighten-2">
-            Gallery
-          </Link>
-        </li>
-        <li>
-          <Link to="/Termdates" className="blue-text text-lighten-2">
-            Term Dates
-          </Link>
-        </li>
-        <li>
-          <Link to="/Contactus" className="blue-text text-lighten-2">
-            Contact Us
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    
+        <nav
+          className={fixedNav ? classes + " fixed-nav-top" : classes}
+          style={{ textAlign: "center" }}
+        >
+          <ul className="navList center blue-text text-lighten-4 s12 m12 l12">
+            {fixedNav && (
+              <li >
+                <Link to="/" className="blue-text text-lighten-2">
+                  IMAGE
+                </Link>
+              </li>
+            )}
+            <li>
+              <Link to="/" className="blue-text text-lighten-2">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/Earlyyears" className="blue-text text-lighten-2">
+                Early tyears foundation stage
+              </Link>
+            </li>
+            <li>
+              <Link to="/Ofested" className="blue-text text-lighten-2">
+                Ofsted
+              </Link>
+            </li>
+            <li>
+              <Link to="/Funding" className="blue-text text-lighten-2">
+                Funding
+              </Link>
+            </li>
+            <li>
+              <Link to="/Testimonials" className="blue-text text-lighten-2">
+                Testimonials
+              </Link>
+            </li>
+            <li>
+              <Link to="/Fundraising" className="blue-text text-lighten-2">
+                Fundraising
+              </Link>
+            </li>
+            <li>
+              <Link to="/Gallery" className="blue-text text-lighten-2">
+                Gallery
+              </Link>
+            </li>
+            <li>
+              <Link to="/Termdates" className="blue-text text-lighten-2">
+                Term Dates
+              </Link>
+            </li>
+            <li>
+              <Link to="/Contactus" className="blue-text text-lighten-2">
+                Contact Us
+              </Link>
+            </li>
+          </ul>
+        </nav>
+       
+   
   );
 };
 
